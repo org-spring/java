7b4e4aa023d7bff3d0e97c2bcbc01fcdbf0e3789
org-spring <org.springframework.boot@gmail.com>
2026-03-02 14:33:39 +0530

    Update Father.java

--- src/com/accessSpecifiers/myfamily/Father.java
+++ src/com/accessSpecifiers/myfamily/Father.java
@@ -2,17 +2,11 @@ package com.accessSpecifiers.myfamily;
 
 public class Father {
 
-    private void atm() {
-        System.out.println("atm");
-    }
+    private void atm() { System.out.println("atm"); }
 
     protected void bike() { System.out.println("bike"); }
 
-    public void cycle() {
-        System.out.println("cycle");
-    }
+    public void cycle() { System.out.println("cycle"); }
 
-    void car() {
-        System.out.println("car");
-    }
+    void car() { System.out.println("car"); }
 }
